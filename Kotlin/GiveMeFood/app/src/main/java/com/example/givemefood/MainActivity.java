d3c82ad9cff2ddff65891f0c3fb8cef19cc0073c
ManuelLerchner <manuel.lerchner1111@gmail.com>
2021-08-24 19:27:04 +0200

    fix join button

--- Kotlin/GiveMeFood/app/src/main/java/com/example/givemefood/MainActivity.java
+++ Kotlin/GiveMeFood/app/src/main/java/com/example/givemefood/MainActivity.java
@@ -6,15 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.TextView;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button loginButton;
+    private Button joinButton;
+    private Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        joinButton = findViewById(R.id.joinButton);
+        createButton = findViewById(R.id.createButton);
 
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openJoin();
+            }
+        });
 
 
     }
 
-    public void openLogin() {
-        Intent intent = new Intent(this, Login.class);
+    public void openJoin() {
+        Intent intent = new Intent(this, Join.class);
         startActivity(intent);
     }
 }
